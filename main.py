51c633f995c2996073c9884b7b11a219e09d25e1
KURO <82338818+drmagestad@users.noreply.github.com>
2025-08-20 20:36:41 -0300

    feat: add main script to run metadata extractor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from metadata_analyzer import extract_metadata
 
 if __name__ == "__main__":
-    filepath = "/home/kali/Documents/Python hacking/seccion2/2_7_metadata_analyzer/Descargas/DSCN0010.jpg"
+    filepath = "PATH"
     metadata = extract_metadata(filepath)
     for key, value in metadata.items():
         print(f"{key}: {value}")
