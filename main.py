6a0847b3ba9a1b5cbaf2974f5b696beba9bab50f
Juan Alvarez <82338818+drmagestad@users.noreply.github.com>
2025-11-16 03:02:11 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from metadata_analyzer import extract_metadata
-
-if __name__ == "__main__":
-    filepath = "PATH"
-    metadata = extract_metadata(filepath)
-    for key, value in metadata.items():
-        print(f"{key}: {value}")
